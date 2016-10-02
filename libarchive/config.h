6ee0a53f4f51944a38d5ba7e03f03e623601af68
Steven Noonan <steven@uplinklabs.net>
2016-10-02 06:33:29 -0700

    build: adding build system for game projects
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ libarchive/config.h
@@ -0,0 +1,9 @@
+#if defined(_MSC_VER) || defined(__MINGW32__)
+#include "config_windows.h"
+#elif defined(__linux__)
+#include "config_linux.h"
+#elif defined(__MACH__)
+#include "config_macosx.h"
+#else
+#error "Need a config.h for your platform!"
+#endif
