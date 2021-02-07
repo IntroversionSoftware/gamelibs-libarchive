7e2a78fb46c8557ff5bbd5b9214d5a8be434abc7
Steven Noonan <steven@uplinklabs.net>
2021-02-07 07:15:19 -0800

    add config file for macOS
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libarchive/config.h
+++ libarchive/config.h
@@ -2,6 +2,8 @@
 #include "config_windows.h"
 #elif defined(__linux__)
 #include "config_linux.h"
+#elif defined(__MACH__)
+#include "config_macosx.h"
 #else
 #error "Need a config.h for your platform!"
 #endif
