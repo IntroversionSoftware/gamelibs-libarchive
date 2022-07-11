6399261aeff1b2d3b5d9481896f7c4d287f1f14d
Steven Noonan <steven@uplinklabs.net>
2022-07-10 23:10:09 -0700

    archive_read_open_file: silence MSVC warning
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libarchive/archive_read_open_file.c
+++ libarchive/archive_read_open_file.c
@@ -109,14 +109,14 @@ static la_ssize_t
 file_read(struct archive *a, void *client_data, const void **buff)
 {
 	struct read_FILE_data *mine = (struct read_FILE_data *)client_data;
-	la_ssize_t bytes_read;
+	size_t bytes_read;
 
 	*buff = mine->buffer;
 	bytes_read = fread(mine->buffer, 1, mine->block_size, mine->f);
 	if (bytes_read < mine->block_size && ferror(mine->f)) {
 		archive_set_error(a, errno, "Error reading file");
 	}
-	return (bytes_read);
+	return (la_ssize_t)(bytes_read);
 }
 
 static int64_t
