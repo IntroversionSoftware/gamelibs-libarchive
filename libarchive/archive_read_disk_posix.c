aad6b626ca451df1778fe044882d1dcc3fbac86f
Steven Noonan <steven@uplinklabs.net>
2022-08-12 20:06:17 -0700

    archive_read_disk_posix.h: fix build on macOS
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libarchive/archive_read_disk_posix.c
+++ libarchive/archive_read_disk_posix.c
@@ -52,9 +52,9 @@ __FBSDID("$FreeBSD$");
 #ifdef HAVE_LINUX_FS_H
 #if !__GLIBC_PREREQ(2, 36)
 #include <linux/fs.h>
+#endif
 #elif HAVE_SYS_MOUNT_H
 #include <sys/mount.h>
-#endif
 #endif
 /*
  * Some Linux distributions have both linux/ext2_fs.h and ext2fs/ext2_fs.h.
