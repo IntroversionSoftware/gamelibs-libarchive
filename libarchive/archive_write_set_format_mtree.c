a3ea3d7c84c8192e90d7f54b3c10b08e549f133e
Steven Noonan <steven@uplinklabs.net>
2026-07-21 09:04:51 -0700

    mtree: suppress an unused variable warning
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- libarchive/archive_write_set_format_mtree.c
+++ libarchive/archive_write_set_format_mtree.c
@@ -1618,6 +1618,9 @@ sum_final(struct mtree_writer *mtree, struct reg_info *reg)
 {
 	struct ae_digest digest;
 
+	/* Unused when no ARCHIVE_HAS_* digest is enabled. */
+	(void)digest;
+
 	if (mtree->compute_sum & F_CKSUM) {
 		uint64_t len;
 		/* Include the length of the file. */
